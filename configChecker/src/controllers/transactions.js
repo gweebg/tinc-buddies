820f068c4b7d60eec783a2ca151525444af72bb1
sucodlaranja <joaoandre210@gmail.com>
2024-04-06 12:51:07 +0100

    missing hardcore logic

--- configChecker/src/controllers/transactions.js
+++ configChecker/src/controllers/transactions.js
@@ -1,6 +1,15 @@
-module.exports.makeTransaction = async () => {
+module.exports.makeTransaction = async (
+  inputAmount,
+  type,
+  userID,
+  configID
+) => {
   try {
-    //logic
+    await fetch("http://backend-app:3000/transactions/", {
+      method: "POST",
+      body: JSON.stringify({ inputAmount, type, userID, configID }),
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
     return { error: error.message };
   }
