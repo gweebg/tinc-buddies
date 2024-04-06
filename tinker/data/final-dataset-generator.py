6c267a8f072194a6b94ccaff8ed5a7d335ec0dda
rsaSantos <rs.elsaint@gmail.com>
2024-04-06 20:50:09 +0100

    lstm networks

--- tinker/data/final-dataset-generator.py
+++ tinker/data/final-dataset-generator.py
@@ -6,5 +6,8 @@ df = pd.read_csv('price/hour/modified_gemini_dataset.csv')
 # Read blockchain-dataset.csv
 df2 = pd.read_csv('blockchain.com/blockchain-dataset.csv')
 
+# Rename 'Volume USD' to 'volume'
+df2.rename(columns={'Volume USD': 'volume'}, inplace=True)
+
 # Write the modified DataFrame back to CSV
 df.to_csv('final_dataset.csv', index=False)
