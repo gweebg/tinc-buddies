2c29e74dd744ea2052b303709b32edcd52df312c
sucodlaranja <joaoandre210@gmail.com>
2024-04-06 21:16:18 +0100

    fix seeds

--- seeds.js
+++ seeds.js
@@ -1,58 +1,58 @@
 const seeds = async () => {
-  await fetch("http://localhost:3000/accounts", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: "john doe",
-      balance: 200,
-    }),
-  })
-    .then((res) =>
-      res.ok ? console.log("User successful") : console.log("User not created")
-    )
-    .catch((err) => console.log(err));
+  try {
+    const resUser = await fetch("http://localhost:3000/accounts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "john doe",
+        balance: 200,
+      }),
+    });
 
-  await fetch("http://localhost:3000/configs", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: "million dollar baby",
-      description: "Lots of money ba by",
-      userID: 1,
-      acquired: 100,
-      budget: 100,
-      activated: true,
-    }),
-  })
-    .then((res) =>
-      res.ok
-        ? console.log("Config successful")
-        : console.log("Config not created")
-    )
-    .catch((err) => console.log(err));
+    resUser.ok
+      ? console.log("User successful")
+      : console.log("User not created");
 
-  await fetch("http://localhost:3000/transactions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      userID: 1,
-      config: { id: 10 },
-      inputAmount: 100,
-      type: "BUY",
-    }),
-  })
-    .then((res) =>
-      res.ok
-        ? console.log("Transaction successful")
-        : console.log("Transaction not created")
-    )
-    .catch((err) => console.log(err));
+    const resConfig = await fetch("http://localhost:3000/configs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: "million dollar baby",
+        description: "Lots of money ba by",
+        userID: 1,
+        acquired: 100,
+        budget: 100,
+        activated: true,
+      }),
+    });
+
+    resConfig.ok
+      ? console.log("Config successful")
+      : console.log("Config not created");
+
+    const resTxns = await fetch("http://localhost:3000/transactions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userID: 1,
+        config: { id: 10 },
+        inputAmount: 100,
+        type: "BUY",
+      }),
+    });
+
+    resTxns.ok
+      ? console.log("Transaction successful")
+      : console.log("Transaction not created");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 seeds();
