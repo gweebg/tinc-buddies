990d38bd9d2412ed8404d913999dd1e2fd746ed5
LCMJ21 <jorge.lima.ne@gmail.com>
2024-04-07 07:36:52 +0100

    stats by user

--- backend/src/accounts/accounts.controller.ts
+++ backend/src/accounts/accounts.controller.ts
@@ -15,4 +15,9 @@ export class AccountsController {
   create(@Body() userData: User): Promise<User> {
     return this.accountsService.create(userData);
   }
+
+  @Get('stats/:id')
+  getStats(@Param('id') id: string): Promise<any> {
+    return this.accountsService.getStats(+id);
+  }
 }
--- backend/src/accounts/accounts.service.ts
+++ backend/src/accounts/accounts.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './accounts.entity';
+import { TransactionsService } from 'src/transactions/transactions.service';
+import { TransactionType } from 'src/transactions/transactions.enum';
 
 @Injectable()
 export class AccountsService {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    private transactionsService: TransactionsService,
   ) {}
 
   findAll(): Promise<User[]> {
@@ -36,5 +39,26 @@ export class AccountsService {
     const user = await this.usersRepository.findOne({ where: { id: userId } });
     user.balance -= ammount;
     return this.usersRepository.save(user);
+  }
+
+  async getStats(userId: number): Promise<any> {
+    const transactions = await this.transactionsService.getTransactionsByUserId(userId);
+    const stats = {
+      profit: 0,
+      numberTransactions: transactions.length,
+      totalBought: 0,
+      totalSold: 0,
+    };
+
+    transactions.forEach((transaction) => {
+      if (transaction.type === TransactionType.BUY) {
+        stats.totalBought += transaction.inputAmount;
+        stats.profit -= transaction.inputAmount;
+      } else {
+        stats.totalSold += transaction.outputAmount;
+        stats.profit += transaction.inputAmount;
+      }
+    });
+    return stats;
   }
 }
