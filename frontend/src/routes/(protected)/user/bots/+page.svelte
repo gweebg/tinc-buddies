1ae80142b75d8a77c67f973271c19ea6212c0e5a
sucodlaranja <joaoandre210@gmail.com>
2024-04-07 06:23:35 +0100

    handle stop and starrt

--- frontend/src/routes/(protected)/user/bots/+page.svelte
+++ frontend/src/routes/(protected)/user/bots/+page.svelte
@@ -18,9 +18,9 @@
 	<div class="container">
 		<header class="mb-4 flex flex-row items-center justify-between">
 			<div>
-				<h1 class="scroll-m-20 text-4xl font-bold tracking-tight">My Bots</h1>
+				<h1 class="scroll-m-20 text-4xl font-bold tracking-tight">My Buddies</h1>
 				<p class="text-muted-foreground text-balance text-lg">
-					See and configure your bots in this page!
+					See and configure your Buddies in this page!
 				</p>
 			</div>
 			<Dialog.Root>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,6 +2,17 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
-<div class="flex h-screen flex-col">
-	<Navbar section="Home"></Navbar>
+<div class="relative my-4 flex flex-1 flex-col gap-4 pb-4">
+	<Navbar section="Home" />
+
+	<div class="container">
+		<header class="mb-4 flex flex-row items-center justify-between">
+			<div>
+				<h1 class="scroll-m-20 text-4xl font-bold tracking-tight">Home</h1>
+				<p class="text-muted-foreground text-balance text-lg">
+					Welcome to Tinc Buddy, your personal assistant for managing your tinc network!
+				</p>
+			</div>
+		</header>
+	</div>
 </div>
